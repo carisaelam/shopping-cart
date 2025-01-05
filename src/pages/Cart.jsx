ab36e9f9d4adecd8b60ed9475abcd51f9bb1e7bf
carisaelam <carisaelam@gmail.com>
2025-01-05 11:13:03 -0500

    Add tests for Cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,12 +4,12 @@ import CartCard from '../components/CartCard/CartCard';
 export default function Cart() {
   const { itemsInCart, removeFromCart } = useCart();
 
-  console.log('itemsInCart', itemsInCart);
-
   return (
     <div>
       <h1>Your Cart</h1>
-      <p>You have {itemsInCart.length} items in your cart</p>
+      <p data-testid="cart__message">
+        You have {itemsInCart.length} items in your cart
+      </p>
       <div>
         {itemsInCart.map((item) => {
           return (
---
+++ src/pages/Cart.test.jsx
@@ -0,0 +1,77 @@
+import { it, describe, expect, vi, beforeEach } from 'vitest';
+import Cart from './Cart';
+import { render, screen } from '@testing-library/react';
+import { RouterProvider, createMemoryRouter } from 'react-router';
+import { CartProvider, useCart } from '../context/CartContext';
+
+vi.mock('../context/CartContext', () => ({
+  useCart: vi.fn(),
+  // eslint-disable-next-line react/prop-types
+  CartProvider: ({ children }) => <div>{children}</div>,
+}));
+
+
+describe('Cart Page', () => {
+  const router = createMemoryRouter(
+    [
+      {
+        path: '/cart',
+        element: (
+          <CartProvider>
+            <Cart />
+          </CartProvider>
+        ),
+      },
+    ],
+    { initialEntries: ['/cart'] }
+  );
+
+  beforeEach(() => {
+    vi.clearAllMocks(); // Ensures previous test mocks do not affect new tests
+  });
+
+  it('should render the Cart Page', () => {
+    useCart.mockReturnValue({
+      addToCart: vi.fn(),
+      removeFromCart: vi.fn(),
+      itemsInCart: [
+        { id: 1, title: 'Product 1', price: 10 },
+        { id: 2, title: 'Product 2', price: 20 },
+      ],
+    });
+
+    render(<RouterProvider router={router} />);
+
+    const cartText = screen.getByRole('heading', { name: /your cart/i });
+    expect(cartText).toBeInTheDocument();
+  });
+
+  it('should correctly show number of items in cart', () => {
+    useCart.mockReturnValue({
+      addToCart: vi.fn(),
+      removeFromCart: vi.fn(),
+      itemsInCart: [
+        { id: 1, title: 'Product 1', price: 10 },
+        { id: 2, title: 'Product 2', price: 20 },
+      ],
+    });
+
+    render(<RouterProvider router={router} />);
+
+    const cartMessage = screen.getByTestId('cart__message');
+    expect(cartMessage).toHaveTextContent(/2/i);
+  });
+
+  it('should show message when cart is empty', () => {
+    useCart.mockReturnValue({
+      addToCart: vi.fn(),
+      removeFromCart: vi.fn(),
+      itemsInCart: [],
+    });
+
+    render(<RouterProvider router={router} />);
+
+    const cartMessage = screen.getByTestId('cart__message');
+    expect(cartMessage).toHaveTextContent(/0/i);
+  });
+});
